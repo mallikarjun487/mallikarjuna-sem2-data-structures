136d9bf87be2db54066cc39c9fa9649831aae914
mallikarjun487 <157685930+mallikarjun487@users.noreply.github.com>
2024-02-03 10:23:24 +0530

    sum_of_elements .c

--- sum of all elements .c
+++ sum of all elements .c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main(){
-	int a[10];
+	int arr[10];
 	int i,n,sum=0;
 	printf("Enter the values : ");
-	scanf("%d",&n);
 	for(i=0;i<=5;i++){
-		scanf("%d",&a[i]);
+		scanf("%d",&arr[i]);
 	}
 	for(i=0;i<=5;i++){
-		sum+=a[i];
+		sum+=arr[i];
 	}
 	printf("sum of the elements in an array :%d",sum);
 	return 0;
