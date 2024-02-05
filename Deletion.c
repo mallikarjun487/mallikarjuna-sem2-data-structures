b9f01337983f1a655322878f30f5b31977923043
mallikarjun487 <157685930+mallikarjun487@users.noreply.github.com>
2024-02-05 18:05:54 +0530

    Add files via upload

---
+++ Deletion.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+ int i,n,k,a[10];
+   for(i=0;i<5;i++){
+     scanf("%d",&a[i]);
+     }
+   printf("enter index:");
+   scanf("%d",&n);
+   if(n>=0&&n<6){
+   printf("enter number:");
+   scanf("%d",&k);
+   for(i=5;i>=n;i--){
+   a[i+1]=a[i];
+   }
+   a[n]=k;
+   for(i=0;i<6;i++){
+     printf(" %d",a[i]);
+     }
+     }
+     else
+      printf("invalid");
+     return 0;
+     }
---
+++ Insertion.c
@@ -0,0 +1,16 @@
+#include<stdio.h>   
+int main(){
+ int i,n,k,a[10];
+   for(i=0;i<5;i++){
+     scanf("%d",&a[i]);
+     }
+   printf("enter index:");
+   scanf("%d",&n);
+    for(i=n;i<4;i++){
+     a[n]=a[n+1];
+     }
+   for(i=0;i<4;i++){
+     printf(" %d",a[i]);
+     }
+     return 0;
+     }
