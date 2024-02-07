7dc0215a54aec3fdf6b94adc78f7d357800377e7
mallikarjun487 <157685930+mallikarjun487@users.noreply.github.com>
2024-02-07 23:13:48 +0530

    Insertion.c

--- Insertion.c
+++ Insertion.c
@@ -1,4 +1,4 @@
-#include<stdio.h>   
+#include<stdio.h>
 int main(){
  int i,n,k,a[10];
    for(i=0;i<5;i++){
@@ -6,11 +6,19 @@ int main(){
      }
    printf("enter index:");
    scanf("%d",&n);
-    for(i=n;i<4;i++){
-     a[n]=a[n+1];
-     }
-   for(i=0;i<4;i++){
+   if(n>=0&&n<6){
+   printf("enter number:");
+   scanf("%d",&k);
+   for(i=5;i>=n;i--){
+   a[i+1]=a[i];
+   }
+   a[n]=k;
+   for(i=0;i<6;i++){
      printf(" %d",a[i]);
      }
+     }
+     else
+      printf("invalid");
      return 0;
      }
+    
