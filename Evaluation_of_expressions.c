e7b20ec95d1e72822663950d9eb49156e7ddb8d1
mallikarjun487 <157685930+mallikarjun487@users.noreply.github.com>
2024-02-21 20:45:09 +0530

    Evaluation_of_expressions.c

--- Evaluation_of_expressions.c
+++ Evaluation_of_expressions.c
@@ -1,32 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// Function to evaluate expressions
 double evaluateExpression(char expression[]) {
     char operators[100];
     double operands[100];
     int opCount = 0;
     int numCount = 0;
-
     for (int i = 0; expression[i] != '\0'; i++) {
         if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/') {
             operators[opCount++] = expression[i];
         } else if (expression[i] >= '0' && expression[i] <= '9') {
-            // Convert character to integer
             double num = expression[i] - '0';
-
-            // Continue reading digits to get the complete number
             while (expression[i + 1] >= '0' && expression[i + 1] <= '9') {
                 num = num * 10 + (expression[i + 1] - '0');
                 i++;
             }
-
             operands[numCount++] = num;
         }
     }
-
     double result = operands[0];
-
     for (int i = 0; i < opCount; i++) {
         switch (operators[i]) {
             case '+':
@@ -51,27 +42,19 @@ double evaluateExpression(char expression[]) {
                 exit(EXIT_FAILURE);
         }
     }
-
     return result;
 }
-
 int main() {
     char expression[100];
-
     printf("Enter an expression: ");
     fgets(expression, sizeof(expression), stdin);
-
-    // Remove newline character from the input
     for (int i = 0; expression[i] != '\0'; i++) {
         if (expression[i] == '\n') {
             expression[i] = '\0';
             break;
         }
     }
-
     double result = evaluateExpression(expression);
-
     printf("Result: %lf\n", result);
-
     return 0;
 }
